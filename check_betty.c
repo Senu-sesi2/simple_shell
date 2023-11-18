a0106ca2a085ab1e00bbe06fbaeebad3bd85bc13
Senu-e30 <senu.emmanuel1993@gmail.com>
2023-11-18 05:52:15 +0000

    update

---
+++ check_betty.c
@@ -0,0 +1,18 @@
+/**
+ * Desc: A beautiful code that passes the Betty checks
+ * Author: Emmanuel Senu
+ * Date: November 18, 2023
+ */
+
+#include <stdio.h>
+
+/**
+ * main - The main function.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Hello, check Betty!\n");
+	return (0);
+}
+
